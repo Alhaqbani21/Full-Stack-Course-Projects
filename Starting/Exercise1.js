8b44b86c02d9f71f044abe6642be95be29f87d33
Abdulaziz <alhaqbani21@gmail.com>
2023-06-06 13:46:58 +0300

    Initialize

---
+++ Starting/Exercise1.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+  console.log('5')
+}, 1000)
+
+setImmediate(() => {
+  console.log('4')
+})
+
+setTimeout(() => {
+  console.log('2')
+}, 0)
+
+process.nextTick(() => {
+  console.log('3')
+})
+
+setTimeout(() => {
+  console.log('1')
+}, 500)
